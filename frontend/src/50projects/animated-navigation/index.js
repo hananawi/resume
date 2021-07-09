c6e455b849422fad6056c23c7daf6797914d59a1
hananawi <1484740339@qq.com>
2021-07-09 21:51:03 +0800

    init

--- frontend/src/50projects/animated-navigation/index.js
+++
@@ -1,9 +0,0 @@
-function f() {
-    const button = document.querySelector('.animated-button');
-    button.addEventListener('click', () => {
-        const nav = document.querySelector('section nav');
-        nav.classList.toggle('active');
-    });
-}
-
-export default f;
--- frontend/test.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-const fetch = require('fetch');
-
-const url = "https://www.iban.com/exchange-rates";
-fetch(url).then(res => {
-  console.log(res);
-});
